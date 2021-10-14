4648ce3cf87320974f4d74785bd43df06bcf3118
Mirjam <hingeke@gmail.com>
2021-10-14 09:02:11 +0300

    Teksti väljastamine ja kommenteerimine

---
+++ prog_alused/app.js
@@ -0,0 +1,9 @@
+
+// konsooli kasutamine
+// console.log('Kommentaarid');
+console.log("Tere, proge alused!");
+console.log('See on teine rida');
+console.log('See ' + 'on ' + 'mingi lause.');
+/*Mitmerealine kommentaar
+* console.log('See on teine rida');
+console.log('See ' + 'on ' + 'mingi lause.');*/
