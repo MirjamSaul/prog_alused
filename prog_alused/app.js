9732698faadea9b7ef284cf4686bb0f13a5e2a83
Mirjam <hingeke@gmail.com>
2021-10-14 09:09:06 +0300

    Teksti väljastamine ja kommenteerimine

--- prog_alused/app.js
+++ prog_alused/app.js
@@ -7,3 +7,5 @@ console.log('See ' + 'on ' + 'mingi lause.');
 /*Mitmerealine kommentaar
 * console.log('See on teine rida');
 console.log('See ' + 'on ' + 'mingi lause.');*/
+
+console.log('Aastal 2016 Eesti presidendiks valiti Kersti Kaljulaid');
