5e1d6e0c615f50cd2e5524e0058bbf9307175ab7
Mirjam <hingeke@gmail.com>
2021-10-25 23:18:18 +0300

    ylesanded:astendamine, trahv, ajakulu

---
+++ prog_alused/ylesanded26okt/ylesanne1-3.js
@@ -0,0 +1,26 @@
+let astmeAlus = prompt("Sisesta astme alus:");
+let astendaja = prompt("Sisestage astendaja");
+let tulemus = astmeAlus**astendaja;
+console.log(tulemus);
+alert(tulemus);
+
+//trahv
+let nimi = prompt("Palun sisesta oma nimi:");
+let lubatudKiirus = prompt("Sisesta lubatud kiirus");
+let tegelikKiirus = prompt("Sisesta tegelik kiirus");
+
+let arvutatud = (tegelikKiirus - lubatudKiirus) * 3;
+//console.log(arvutatud);
+
+alert(nimi + ", " + "sinu trahv on " + Math.min(190, arvutatud) + " eurot.");
+console.log(nimi + ", " + "sinu trahv on " + Math.min(190, arvutatud) + " eurot.");
+
+//Ajakulu
+let ainepunktid = prompt("Kirjuta ainepunktide arv:");
+let nadalateArv = prompt("Kirjuta nädalate arv;");
+let ajakulu = Math.trunc((ainepunktid * 26) / nadalateArv);
+console.log(ajakulu);
+alert(ajakulu);
+
+
+
