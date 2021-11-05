80aa73b77068dd24c3923e37ac40d169043bef03
Mirjam <hingeke@gmail.com>
2021-11-05 07:48:57 +0200

    tsüklid.lisatid õunte jagamise yl

--- prog_alused/tsüklid-ylesanded/tsyklid-ylesanded.js
+++ prog_alused/tsüklid-ylesanded/tsyklid-ylesanded.js
@@ -41,6 +41,24 @@ for (let lugeja = 1; lugeja <= taringud; lugeja++) { //tsükkel jookseb vastaval
     console.log("Viske tulemus on: " + (Math.floor(Math.random() * 6) + 1)); //1-6
 }
 
+//õunte jagamine
+let mituPp = prompt("Mitu pöialpoissi tahab õunu?");
+
+if (mituPp === "1") {
+    console.log((mituPp + " pöialpoiss soovib õunu."));
+} else {console.log(mituPp + " pöialpoissi soovib õunu.")}
+
+let ounadKokku = 0;
+
+for (let ppi = 0; ppi < mituPp; ppi++) {
+    let loosOun = Math.floor(Math.random() * 2) + 1;
+
+    console.log("Loosi tulemused: " + loosOun);
+    ounadKokku = loosOun + ounadKokku;
+
+}
+console.log("Lumivalgekesele jääb " + (14 - ounadKokku));
+
 
 
 
