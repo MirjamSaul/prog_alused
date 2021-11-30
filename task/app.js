0894937b0410544f4aca0b11492ff1402b922ce2
Mirjam <hingeke@gmail.com>
2021-11-30 16:18:55 +0200

    Delete singel task

--- task/app.js
+++ task/app.js
@@ -4,14 +4,28 @@
 const form = document.querySelector('form');
 form.addEventListener('submit', addTask);
 
-console.log(form);
+//tasklist
+const taskList = document.querySelector('ul');
+taskList.addEventListener('click', delTask);
+
+//delTask
+function delTask(event) {
+    //console.log(event.target.TextContent);
+    if(event.target.textContent === 'X') {
+        if(confirm('Do you relly want to delete this task?')) {
+            event.target.parentElement.remove();
+        } //prompt funkts true-false
+        //console.log(event.target.parentElement);
+    }
+}
+//console.log(form);
 
 function addTask(event){
     //get task input from form input
     const task = document.querySelector('#task').value;
     console.log(event.type);
     //get element from DOM
-    const taskList = document.querySelector('ul');
+
     //create element to DOM
     const li = document.createElement('li');
 
@@ -20,6 +34,18 @@ function addTask(event){
     //add text to element
     const text = document.createTextNode(task);
     li.appendChild(text);
+
+    //kustutamine
+    // loo element, lisa ...
+    const link = document.createElement('a');
+    //add css style
+    link.className = 'secondary-content';
+    //add text
+    link.appendChild(document.createTextNode('X'))
+    //add link to list item
+    li.appendChild(link);
+   //add href attribute
+    link.setAttribute('href', '#');
     //add li to task list
     taskList.appendChild(li);
     //clear form input value
