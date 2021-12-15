1f5be45969792a65ae3e33ba344ffc24e61833c6
Mirjam <hingeke@gmail.com>
2021-12-15 12:34:03 +0200

    Fetched city weather data from API

--- weather/app.js
+++ weather/app.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded', cityWeather);
+
 function wDataFetch(city) {
     let key = 'be362d70a298f4dccb97270a77803eee';
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
@@ -6,9 +8,9 @@ function wDataFetch(city) {
         })
         .then(function(data) {
             console.log(data);
+            drawWeather(data);
         })
         .catch(function() {
-
         })
 }
 
@@ -16,4 +18,18 @@ function cityWeather() {
     wDataFetch('Tartu');
 
 }
-cityWeather();
+
+function drawWeather(data) {
+    let celsius = Math.round(parseFloat(data.main.temp)-273.15);
+    let feelsLikeCelsius = Math.round(parseFloat(data.main.feels_like)-273.15);
+    let description = data.weather[0].description;
+
+
+    document.querySelector('#description').innerHTML = description;
+    document.querySelector('#temp').innerHTML = celsius + '&deg';
+    document.querySelector('#location').innerHTML = data.name;
+    document.querySelector('#feelsLike').innerHTML = feelsLikeCelsius;
+}
+
+
+
