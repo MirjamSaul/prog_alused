aa220fb7dc237091364c53711d32f209a4d5d47c
Mirjam <hingeke@gmail.com>
2022-01-06 09:16:03 +0200

    Populated data structure items to list

--- CaloryTracker/app.js
+++ CaloryTracker/app.js
@@ -19,16 +19,36 @@ const ItemCtrl = (function() {
         total: 0
     }
     return {
+        getItems: function(){
+            return data.items
+        },
         logData: function(){
-            return data
+            return data;
+
         }
     }
 })();
 
 //UI Controller
 const UICtrl = (function() {
-    console.log('UICtrl')
+    return {
+        populateItemList: function(items){
+            //create html content
+            let html = '';
 
+            //parse data and create list items in html
+            items.forEach(function(item){
+                html += `<li class="collection-item" id="item-${item.id}">
+                    <strong>${item.name}: </strong> <em>${item.calories}Calories</em>
+                    <a href="#" class="secondary-content">
+                        <i class="edit-item fa fa-pencil"></i>
+                    </a>
+                </li>`
+            });
+            //insert list items
+            document.querySelector("#item-list").innerHTML = html;
+        }
+    }
 })();
 
 // APP Controller
@@ -37,6 +57,10 @@ const App = (function(ItemCtrl, UICtrl){
     return {
         init: function(){
             console.log('Initializing App')
+            //fetch items from data structure
+            const items = ItemCtrl.getItems()
+            //populate items list
+            UICtrl.populateItemList(items)
         }
     }
 })(ItemCtrl, UICtrl);
