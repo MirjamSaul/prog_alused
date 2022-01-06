e7fdd0b48c8e7b201864e1531403a4dc7dd49a5d
Mirjam <hingeke@gmail.com>
2022-01-06 09:23:55 +0200

    Refactored poulated data structure items to list

--- CaloryTracker/app.js
+++ CaloryTracker/app.js
@@ -31,6 +31,10 @@ const ItemCtrl = (function() {
 
 //UI Controller
 const UICtrl = (function() {
+    //UI selectors
+    const UISelectors = {
+        itemList: '#item-list'
+    }
     return {
         populateItemList: function(items){
             //create html content
@@ -46,7 +50,7 @@ const UICtrl = (function() {
                 </li>`
             });
             //insert list items
-            document.querySelector("#item-list").innerHTML = html;
+            document.querySelector(UISelectors.itemList).innerHTML = html;
         }
     }
 })();
